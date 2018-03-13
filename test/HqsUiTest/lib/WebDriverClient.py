254883d80fc94e22bbced294f05614f72653040e
zimengrao <18202956488@163.com>
2018-03-13 18:37:15 +0800

    好琴声UI测试

--- test/HqsUiTest/lib/WebDriverClient.py
+++ test/HqsUiTest/lib/WebDriverClient.py
@@ -7,3 +7,26 @@
 """
 
 from selenium import webdriver
+from lxml.html import fromstring
+
+class WebDriver:
+    def __init__(self):
+        self.driver = self.init_driver()
+
+    def init_driver(self):
+        return webdriver.Firefox()
+
+    def get(self, url):
+        self.driver.get(url)
+
+    def parse(self, xpath):
+        return fromstring(self.driver.page_source).xpath(xpath)
+
+    def click(self, xpath):
+        self.driver.find_element_by_xpath(xpath).click()
+
+    def send_keys(self, xpath, value):
+        self.driver.find_element_by_xpath(xpath).send_keys(value)
+
+    def check(self, xpath):
+        return bool(self.parse(xpath))
