73c096dba9e71848144a9c3f82a262986e0e4f83
zimengrao <18202956488@163.com>
2018-04-20 18:43:59 +0800

    音乐广场接口

--- test/GTApiTest/config/__init__.py
+++ test/GTApiTest/config/__init__.py
@@ -11,31 +11,30 @@ class Config:
     enum = {
         'GTApi': {
             'base_url': 'http://test.greattone.net/e/appapi/',
-            'login': {
-                'data': {
+            'login': [
+                {
                     'api': 'user/login'  # 用户名密码为空
                 },
-                'data1': {
+                {
                     'api': 'user/login',  # 密码为空
                     'username': '读后感客户端上老师'
                 },
-                'data2': {
+                {
                     'api': 'user/login',  # 用户名为空
                     'password': '565699'
                 },
-                'data3': {
+                {
                     'api': 'user/login',  # 正确的用户名，错误的密码
                     'username': '贾老师',
                     'password': '565699'
                 },
-                'data4': {
+                {
                     'api': 'user/login',  # 错误的用户名，错误的密码
                     'username': '贾老师',
                     'password': '123456'
-                },
-            },
-            'kdkdk': 'oo'
-        },
+                }
 
+            ],
+            'guangchang': '?api=info/guanlist&classid=1&extra=shipin,hai_id,hai_video,hai_photo,hai_petition,hai_name&pageIndex=1&pageSize={}&loginuid={}&logintoken={}&timehot='
+        }
     }
-
---
+++ test/GTApiTest/tests/test_guangchang_api.py
@@ -0,0 +1,12 @@
+"""
+@Name: test_guangchang_api.py
+@Version: 
+@Project: PyCharm
+@Author: wangmin
+@Data: 2018/4/20
+"""
+
+import json
+import unittest
+from lib.client import HttpHandler
+from config import Config
